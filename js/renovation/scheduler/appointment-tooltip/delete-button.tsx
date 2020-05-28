483636094839ea7f38f76ea36de9ae73270f7513
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-05-28 15:08:53 +0300

    Test and refactor delete-button

--- js/renovation/scheduler/appointment-tooltip/delete-button.tsx
+++ js/renovation/scheduler/appointment-tooltip/delete-button.tsx
@@ -1,27 +1,22 @@
 import {
-  Component, ComponentBindings, JSXComponent,
+  Component, ComponentBindings, JSXComponent, Event,
 } from 'devextreme-generator/component_declaration/common';
-import { ButtonProps } from '../../button';
-import Button from '../../wrapped-button';
+import Button from '../../button';
 import {
   TOOLTIP_APPOINTMENT_ITEM_DELETE_BUTTON,
 } from './consts';
 
-export const viewFunction = ({
-  restAttributes,
-  props: {
-    onClick,
-  },
-}: DeleteButton) => (
+export const viewFunction = (viewModel: DeleteButton) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <div className={TOOLTIP_APPOINTMENT_ITEM_DELETE_BUTTON} {...restAttributes}>
-    <Button icon="trash" stylingMode="text" onClick={onClick} />
+  <div className={TOOLTIP_APPOINTMENT_ITEM_DELETE_BUTTON} {...viewModel.restAttributes}>
+    <Button icon="trash" stylingMode="text" onClick={viewModel.props.onClick} />
   </div>
-
 );
 
 @ComponentBindings()
-export class DeleteButtonProps extends ButtonProps {}
+export class DeleteButtonProps {
+  @Event() onClick?: (e: any) => void;
+}
 
 @Component({
   defaultOptionRules: null,
